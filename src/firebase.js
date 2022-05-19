d5e2d02b368b31ff397947501057a721f6a362a0
daddyreggie <daddyreggie8@gmail.com>
2022-05-19 14:16:02 +0300

    deleted svg logo icon

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCADNKo3nfV1qVZRx65VzfrDvemupLXvs0",
+    authDomain: "e-clone-24fb7.firebaseapp.com",
+    projectId: "e-clone-24fb7",
+    storageBucket: "e-clone-24fb7.appspot.com",
+    messagingSenderId: "612098689164",
+    appId: "1:612098689164:web:887ee6ef03163a173fd3a9",
+    measurementId: "G-0FV9XH75M1"
+  };
